81ae89d57a981a8b40176c6f4344cb04f06ca830
csynoers <3s0c9m7@gmail.com>
2020-04-12 23:26:01 +0700

    title page website default

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Admin</title>
+  <title>Try Out CAT CPNS | Bimbel IC Surabaya</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="<?= base_url('favicon.ico') ?>" type="image/gif" sizes="16x16">
